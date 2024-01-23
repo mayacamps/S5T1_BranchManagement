6fd2cfbd6b031f7d74e1e6152898221a0c39b995
Maya <maya.camps@estudiantat.upc.edu>
2024-01-23 10:54:14 +0100

    initial commit·

---
+++ src/main/java/cat/itacademy/barcelonactiva/camps/maya/s05/t01/n01/S05T01N01CampsMaya/S05T01N01CampsMayaApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.camps.maya.s05.t01.n01.S05T01N01CampsMaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01N01CampsMayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01N01CampsMayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/barcelonactiva/camps/maya/s05/t01/n01/S05T01N01CampsMaya/S05T01N01CampsMayaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.camps.maya.s05.t01.n01.S05T01N01CampsMaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N01CampsMayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
