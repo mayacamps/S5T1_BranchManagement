3700814764f1aa1a4fa26ed69cecaf60f9135686
Maya <maya.camps@estudiantat.upc.edu>
2024-02-07 13:45:59 +0100

    exception handler - wrongURl

--- src/main/java/cat/itacademy/barcelonactiva/camps/maya/s05/t01/n01/S05T01N01CampsMaya/exceptions/GlobalExceptionHandler.java
+++ src/main/java/cat/itacademy/barcelonactiva/camps/maya/s05/t01/n01/S05T01N01CampsMaya/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package cat.itacademy.barcelonactiva.camps.maya.s05.t01.n01.S05T01N01CampsMaya.e
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
@@ -16,4 +17,8 @@ public class GlobalExceptionHandler {
         return "redirect:/api/v1/";
     }
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public String MethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex){
+        return "redirect:/api/v1/";
+    }
 }
